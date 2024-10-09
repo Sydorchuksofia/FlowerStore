977f4376e77fb1d6a2086ff73617b69d50f96190
Sydorchuk Sofia <sydorchuk.pn@ucu.edu.ua>
2024-10-09 15:42:48 +0300

    fix 11

--- src/test/java/flower/store/StoreTest.java
+++ src/test/java/flower/store/StoreTest.java
@@ -8,83 +8,96 @@ import java.util.List;
 
 public class StoreTest {
 
-    private static final double ROSE_SEPAL_LENGTH = 5.0;
-    private static final double ROSE_PRICE = 10.0;
-    private static final int ROSE_COUNT = 5;
+        private static final double ROSE_SEPAL_LENGTH = 5.0;
+        private static final double ROSE_PRICE = 10.0;
+        private static final int ROSE_COUNT = 5;
 
-    private static final double TULIP_SEPAL_LENGTH = 6.0;
-    private static final double TULIP_PRICE = 15.0;
-    private static final int TULIP_COUNT = 2;
+        private static final double TULIP_SEPAL_LENGTH = 6.0;
+        private static final double TULIP_PRICE = 15.0;
+        private static final int TULIP_COUNT = 2;
 
-    private static final int EXPECTED_ROSE_SEARCH_SIZE = 1;
-    private static final int EXPECTED_NO_FLOWER_FOUND_SIZE = 0;
+        private static final int EXPECTED_ROSE_SEARCH_SIZE = 1;
+        private static final int EXPECTED_NO_FLOWER_FOUND_SIZE = 0;
 
-    private Store store;
+        private Store store;
 
-    @BeforeEach
-    public void init() {
-        store = new Store();
-    }
+        @BeforeEach
+        public void init() {
+                store = new Store();
+        }
 
-    @Test
-    public void testAddBucket() {
-        Flower flower = new Rose(ROSE_SEPAL_LENGTH,
-                FlowerColor.RED, ROSE_PRICE);
-        FlowerPack flowerPack = new FlowerPack(flower,
-                ROSE_COUNT);
-        FlowerBucket flowerBucket = new FlowerBucket();
-        flowerBucket.add(flowerPack);
+        private FlowerBucket createFlowerBucket(FlowerType flowerType,
+                        double sepalLength,
+                        FlowerColor color,
+                        double price,
+                        int count) {
+                Flower flower;
+                switch (flowerType) {
+                        case ROSE:
+                                flower = new Rose(sepalLength, color, price);
+                                break;
+                        case TULIP:
+                                flower = new Tulip(sepalLength, color, price);
+                                break;
+                        default:
+                                throw new IllegalArgumentException("Unknown");
+                }
+                FlowerPack flowerPack = new FlowerPack(flower, count);
+                FlowerBucket flowerBucket = new FlowerBucket();
+                flowerBucket.add(flowerPack);
+                return flowerBucket;
+        }
 
-        store.addBucket(flowerBucket);
+        @Test
+        public void testAddBucket() {
+                FlowerBucket flowerBucket = createFlowerBucket(FlowerType.ROSE,
+                                ROSE_SEPAL_LENGTH,
+                                FlowerColor.RED,
+                                ROSE_PRICE,
+                                ROSE_COUNT);
+                store.addBucket(flowerBucket);
 
-        Assertions.assertEquals(EXPECTED_ROSE_SEARCH_SIZE,
-                store.search(FlowerType.ROSE).size());
-    }
+                Assertions.assertEquals(EXPECTED_ROSE_SEARCH_SIZE,
+                                store.search(FlowerType.ROSE).size());
+        }
 
-    @Test
-    public void testSearchFlowerType() {
-        Flower rose = new Rose(ROSE_SEPAL_LENGTH,
-                FlowerColor.RED, ROSE_PRICE);
-        Flower tulip = new Tulip(TULIP_SEPAL_LENGTH,
-                FlowerColor.YELLOW, TULIP_PRICE);
+        @Test
+        public void testSearchFlowerType() {
+                FlowerBucket bucketOne = createFlowerBucket(FlowerType.ROSE,
+                                ROSE_SEPAL_LENGTH,
+                                FlowerColor.RED,
+                                ROSE_PRICE,
+                                ROSE_COUNT);
+                FlowerBucket bucketTwo = createFlowerBucket(FlowerType.TULIP,
+                                TULIP_SEPAL_LENGTH,
+                                FlowerColor.YELLOW,
+                                TULIP_PRICE,
+                                TULIP_COUNT);
 
-        FlowerPack rosePack = new FlowerPack(rose,
-                ROSE_COUNT);
-        FlowerPack tulipPack = new FlowerPack(tulip,
-                TULIP_COUNT);
+                store.addBucket(bucketOne);
+                store.addBucket(bucketTwo);
 
-        FlowerBucket bucketOne = new FlowerBucket();
-        bucketOne.add(rosePack);
+                List<FlowerBucket> foundBuckets = store.search(FlowerType.ROSE);
 
-        FlowerBucket bucketTwo = new FlowerBucket();
-        bucketTwo.add(tulipPack);
+                Assertions.assertEquals(EXPECTED_ROSE_SEARCH_SIZE,
+                                foundBuckets.size());
+                Assertions.assertEquals(FlowerType.ROSE,
+                                foundBuckets.get(0).getFlowerPacks()
+                                                .get(0).getFlower().getFlowerType());
+        }
 
-        store.addBucket(bucketOne);
-        store.addBucket(bucketTwo);
+        @Test
+        public void testSearchFlowerTypeNotFound() {
+                FlowerBucket flowerBucket = createFlowerBucket(FlowerType.ROSE,
+                                ROSE_SEPAL_LENGTH,
+                                FlowerColor.RED,
+                                ROSE_PRICE,
+                                ROSE_COUNT);
+                store.addBucket(flowerBucket);
 
-        List<FlowerBucket> foundBuckets = 
-        store.search(FlowerType.ROSE);
+                List<FlowerBucket> foundBuckets = store.search(FlowerType.TULIP);
 
-        Assertions.assertEquals(EXPECTED_ROSE_SEARCH_SIZE,
-                foundBuckets.size());
-        Assertions.assertEquals(FlowerType.ROSE,
-foundBuckets.get(0).getFlowerPacks().get(0).getFlower().getFlowerType());
-    }
-
-    @Test
-    public void testSearchFlowerTypeNotFound() {
-        Flower flower = new Rose(ROSE_SEPAL_LENGTH,
-                FlowerColor.RED, ROSE_PRICE);
-        FlowerPack flowerPack = new FlowerPack(flower, ROSE_COUNT);
-        FlowerBucket flowerBucket = new FlowerBucket();
-        flowerBucket.add(flowerPack);
-
-        store.addBucket(flowerBucket);
-
-        List<FlowerBucket> foundBuckets = 
-        store.search(FlowerType.TULIP); 
-
-        Assertions.assertEquals(EXPECTED_NO_FLOWER_FOUND_SIZE,
-                foundBuckets.size());
-    }
+                Assertions.assertEquals(EXPECTED_NO_FLOWER_FOUND_SIZE,
+                                foundBuckets.size());
+        }
 }
